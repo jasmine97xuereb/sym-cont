67ddf6932dfbef6ce493ee18bc43217c9792326b
jasmine97xuereb <jasmine.xuereb.15@um.edu.mt>
2019-12-30 09:54:28 +0100

    updated sym_reduce to take as param c with type list of exp lists

--- Tool/src/definitions/symbolic-controllability/symTransitions.ml
+++ Tool/src/definitions/symbolic-controllability/symTransitions.ml
@@ -5,7 +5,7 @@ open SatFunction
 open PrettyPrint
 
 (*transition rules for the definition of Symbolic Controllability*)
-let rec sym_reduce (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t): monitor_type list =
+let rec sym_reduce (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t list): monitor_type list =
   (match mon_in with
   | Ast.Monitor.Verdict(_) as x ->  
       [REDUCED(x)]
@@ -24,7 +24,7 @@ let rec sym_reduce (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: As
   | Ast.Monitor.Recurse(x) -> 
       rule_srec mon_in b tr_in c)
 
-and rule_tvar  (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t) : monitor_type list =
+and rule_tvar  (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t list) : monitor_type list =
   match mon_in with
   | Ast.Monitor.TVar(x) ->  
     (match TVars.find_opt x.tvar !mapTVar with
@@ -32,7 +32,7 @@ and rule_tvar  (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.Sy
     | None -> [ERROR("Not found in map")])
   | _ -> [ERROR("Incorrect Structure")]
 
-  and rule_schoice (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t) : monitor_type list =
+  and rule_schoice (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t list) : monitor_type list =
     match mon_in with
       | Ast.Monitor.Choice(x) ->
         let reduced_rt = sym_reduce x.right b tr_in c
@@ -49,26 +49,28 @@ and rule_tvar  (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.Sy
         in add (reduced_rt @ reduced_lt) []
       | _ -> [ERROR("Incorrect Structure")]
 
-and rule_sgre (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t): monitor_type list =
+and rule_sgre (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t list): monitor_type list =
   match mon_in with
   | Ast.Monitor.ExpressionGuard(x) ->
     (if (compare_values (reduce_identifier x.label) (reduce_identifier tr_in.label))
     then
       let new_cond = (add_binary_condition x.payload (create_exp_identifier tr_in.payload.name) Ast.Expression.BinaryExp.Compare) in
-      (if sat [add_binary_condition c new_cond Ast.Expression.BinaryExp.And]
+      (* (if sat [add_binary_condition c new_cond Ast.Expression.BinaryExp.And] *)
+      (if sat (c @ [new_cond])
         then [REDUCED(x.consume)]
       else [ERROR("Payload does not match")])
     else [ERROR("Label does not match\n")])
 
   | _ -> [ERROR("Incorrect Structure")]
 
-and rule_sgrq (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t): monitor_type list =
+and rule_sgrq (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t list): monitor_type list =
   match mon_in with
   | Ast.Monitor.QuantifiedGuard(x) ->
     (if (compare_values (reduce_identifier x.label ) (reduce_identifier tr_in.label ))
     then
       let new_cond = (add_binary_condition x.payload x.payload Ast.Expression.BinaryExp.Compare) in
-      (if sat [add_binary_condition c new_cond Ast.Expression.BinaryExp.And]
+      (* (if sat [add_binary_condition c new_cond Ast.Expression.BinaryExp.And] *)
+      (if sat (c @ [new_cond])
         then (*substitute all occurences of x.payload by the symbolic event's payload*)
           let to_consume = inner_sub_eval x.consume x.payload (Ast.Expression.Identifier(tr_in.payload)) 
           in [REDUCED(to_consume)]
@@ -76,21 +78,24 @@ and rule_sgrq (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.Sym
     else [ERROR("Label does not match\n")])
   | _ -> [ERROR("Incorrect Structure")]
 
-and rule_sif (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t): monitor_type list =
+and rule_sif (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t list): monitor_type list =
   match mon_in with
     | Ast.Monitor.Conditional(x) -> (
-        let c_true = add_binary_condition c x.condition Ast.Expression.BinaryExp.And in 
+        (* let c_true = add_binary_condition c x.condition Ast.Expression.BinaryExp.And in 
         let c_false = add_binary_condition c (add_unary_condition x.condition) Ast.Expression.BinaryExp.And in 
-        if (sat [c_true])
+        if (sat [c_true]) *)
+        let c_true = c @ [x.condition] in 
+        let c_false = c @ [add_unary_condition x.condition] in 
+        if (sat c_true)
         then sym_reduce x.if_true b tr_in c_true
-        else if (sat [c_false])
+        else if (sat c_false)
         then sym_reduce x.if_false b tr_in c_false 
         else [ERROR("unable to reduce")]      
       )
     | _ ->              
         [ERROR("Incorrect structure")]
 
-and rule_slet (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t): monitor_type list =
+and rule_slet (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t list): monitor_type list =
   match mon_in with
     | Ast.Monitor.Evaluate(x) -> (
       (* let new_cond = add_binary_condition c (add_binary_condition x.var x.subst Ast.Expression.BinaryExp.Compare) Ast.Expression.BinaryExp.And in *)
@@ -111,7 +116,7 @@ and rule_slet (mon_in: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.Sym
 (*Upon encountering a recursion monitor, check if it is already in the map
 If it was already in the map, then monvar was already bound, thus continue reducing
 Otherwise, add the monvar (mapping to the consume mon of rec) to the map and continue reducing *)
-and rule_srec (mon: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t): monitor_type list =
+and rule_srec (mon: Ast.Monitor.t) (b: Ast.Expression.t list) (tr_in: Ast.SymbolicEvent.t) (c: Ast.Expression.t list): monitor_type list =
   match mon with
     | Ast.Monitor.Recurse(x) -> 
       (match TVars.find_opt x.monvar.tvar !mapTVar with
