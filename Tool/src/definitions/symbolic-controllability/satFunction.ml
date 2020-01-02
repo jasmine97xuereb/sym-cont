e11d71fc509e5d50e54f7cf5e9c13fef44e256ac
jasmine97xuereb <jasmine.xuereb.15@um.edu.mt>
2020-01-02 20:33:27 +0100

    optimised sat checking using tactics

--- Tool/src/definitions/symbolic-controllability/satFunction.ml
+++ Tool/src/definitions/symbolic-controllability/satFunction.ml
@@ -1,4 +1,5 @@
 open EnvResources
+open EnvFunctions
 open Z3
 open Z3.Symbol
 open Z3.Sort
@@ -7,10 +8,9 @@ open Z3.Boolean
 open Z3.Arithmetic
 open Z3.Arithmetic.Integer
 open Z3.Solver
-
-(*open Z3.Goal
+open Z3.Goal
 open Z3.Tactic
-open Z3.Tactic.ApplyResult*)
+open Z3.Tactic.ApplyResult
 
 (** function to convert an list of expressions in the form Ast.Expression.t into the Z3.Expr.expr required by the Z3 library  *)
 let rec exp_list_to_z3 (c: Ast.Expression.t list) (a: Z3.Expr.expr list) (ctx: context) =
@@ -44,56 +44,16 @@ let rec exp_list_to_z3 (c: Ast.Expression.t list) (a: Z3.Expr.expr list) (ctx: c
     | [] -> (Boolean.mk_and ctx a)
     | e::es -> exp_list_to_z3 es (a @ [single_exp_to_z3 e ctx]) ctx
 
-(* checks whether a list of expressions is satisfiable *)
-let sat (c: Ast.Expression.t list): bool  =
-  let cfg = [("model", "true")] in
-    let ctx = (mk_context cfg) in
-        let solver = (mk_solver ctx None) in
-          let cndts = exp_list_to_z3 c [] ctx in
-            (Solver.add solver [cndts]);
-            (*print_endline(to_string solver);*) 
-            if (check solver []) == SATISFIABLE
-            then true
-            else false
-
-
-let rec print_subgoals (subgoals: Z3.Goal.goal list) = 
-  match subgoals with
-  | [] -> print_endline("finish")
-  | g1::g2 -> 
-    print_endline(Goal.to_string g1);
-    print_subgoals g2
-
-(*let sat (c: Ast.Expression.t list): bool  =
+(* checks whether a list of expressions is satisfiable using tactics *)
+let sat (c: Ast.Expression.t list): bool = 
   let cfg = [("model", "true")] in 
     let ctx = (mk_context cfg) in
-        let solver = (mk_solver ctx None) in
-          let cndts = exp_list_to_z3 c [] ctx in
-            
-            let g = (mk_goal ctx true false false) in
-            (Goal.add g [ cndts ]) ;
-            print_endline("-------------------");
-            print_endline(Goal.to_string g) ;
-
-            (
-              let result = (Tactic.apply (and_then ctx (mk_tactic ctx ("ctx-simplify")) (mk_tactic ctx "propagate-ineqs") []) g None) in
-               (*if there is one subgoal and it is false, then unsat*)
-                (if ((get_num_subgoals result) == 1 && ((is_decided_sat (get_subgoal result 0)) ||  (is_decided_unsat (get_subgoal result 0)))) 
-                then
-                  print_endline("unsat")
-                else (*if there is more than one subgoal, then sat*)
-                  print_endline("sat")
-                );
-                
-                print_endline("subgoals are: ");
-                print_subgoals (get_subgoals result)
-            );
-
-            print_endline("-------------------");
-            if (check solver []) == SATISFIABLE
-            then true
-            else false
-*)
-(*(apply  (then qe ctx-solver-simplify propagate-ineqs))*)
-
-
+      let cndts = exp_list_to_z3 c [] ctx in
+        let g = (mk_goal ctx true false false) in
+          (Goal.add g [cndts]);
+  
+          let result = (Tactic.apply (and_then ctx (mk_tactic ctx ("ctx-solver-simplify")) (mk_tactic ctx "propagate-ineqs") []) g None) in
+            if is_decided_unsat (get_subgoal result 0) 
+            then false
+            else true  
+          
