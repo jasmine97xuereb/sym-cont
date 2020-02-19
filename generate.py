3bdd5a5b5c0e2eb9ca811e99288717145dc1f61a
jasmine97xuereb <jasmine.xuereb.15@um.edu.mt>
2020-02-19 12:02:53 +0100

    gen

--- generate.py
+++ generate.py
@@ -20,29 +20,29 @@ counter = 6+(2*(nb_choice-1))
 
 # print(string)
 
-# for i in range(nb_choice):
-#     if i == 0 and nb_choice == 0:
-#         string2 = "l(x).if x==4 then k<x>.2 else k<x>.1"
-#     else:
-#         temp = "if x mod 2 == 0 then "
-#         counter = 6+(2*(i)) 
-#         for x in range(i+1):
-#             temp = temp + "if x<" + str(counter) + " then " 
-#             counter -= 2
-#         temp = temp + "if x>2 then k<x>.2 else k<x>.1"
-#         for x in range(i+1):
-#             temp = temp + " else k<x>.1"
-#         temp = temp+" else k<x>.1"
-#         string2 = string2 + " + ("+temp+")"
+for i in range(nb_choice):
+    if i == 0 and nb_choice == 0:
+        string2 = "l(x).if x==4 then k<x>.2 else k<x>.1"
+    else:
+        temp = "if x mod 2 == 0 then "
+        counter = 6+(2*(i)) 
+        for x in range(i+1):
+            temp = temp + "if x<" + str(counter) + " then " 
+            counter -= 2
+        temp = temp + "if x>2 then k<x>.2 else k<x>.1"
+        for x in range(i+1):
+            temp = temp + " else k<x>.1"
+        temp = temp+" else k<x>.1"
+        string2 = string2 + " + ("+temp+")"
 
-# print(string2) 
+print(string2) 
 
-for i in range(nb_choice-1):
-    string3 = string3 + "+ (k<" + str(i+2) + ">.(l<" +str(i+2)+ ">.X)+(q<" +str(i+2)+ ">.1))"
+# for i in range(nb_choice-1):
+#     string3 = string3 + "+ (k<" + str(i+2) + ">.(l<" +str(i+2)+ ">.X)+(q<" +str(i+2)+ ">.1))"
 
-string3 = string3 + "+(k<"+ str(nb_choice+1) +">.(l<" +str(nb_choice+1)+ ">.X)+(q<" +str(nb_choice+1)+ ">.1))"    
+# string3 = string3 + "+(k<"+ str(nb_choice+1) +">.(l<" +str(nb_choice+1)+ ">.X)+(q<" +str(nb_choice+1)+ ">.1))"    
 
-print(string3)
+# print(string3)
 
 # for i in range(nb_choice-1):
 #     string4 += "k<(x+" +str(i+1)+ ")>."
