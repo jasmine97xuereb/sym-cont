85de8db3aa551685897b6f8fa2862ea4500b14c3
jasmine97xuereb <jasmine.xuereb.15@um.edu.mt>
2020-02-25 08:41:15 +0100

    eval

--- generate.py
+++ generate.py
@@ -6,46 +6,76 @@ except:
   print("Enter Number of Repetition: ") 
   nb_choice = int(input())
 
-string = "(recX.k<1>.X)"
-string2="l(x).(if x==4 then k<x>.2 else k<x>.1)"
-string3 = "rec X.(k<1>.(l<1>.X)+(q<1>.1))"
-string4 = "l(x).k<x>."
+# string = "(recX.k<1>.X)"
+# string2="l(x).(if x==4 then k<x>.2 else k<x>.1)"
+# string3 = "rec X.(k<1>.(l<1>.X)+(q<1>.1))"
+# string4 = "l(x).k<x>."
 
-counter = 6+(2*(nb_choice-1)) 
+# counter = 6+(2*(nb_choice-1)) 
 
-for i in range(nb_choice-1):
-    string = string + "+ (recX.k<" + str(i+2) + ">.X)"
+# for i in range(nb_choice-1):
+#     string = string + "+ (recX.k<" + str(i+2) + ">.X)"
 
-string = string + "+(recX.k<"+ str(nb_choice+1) +">.X)"    
+# string = string + "+(recX.k<"+ str(nb_choice+1) +">.X)"    
 
-print(string)
+# print(string)
 
-if nb_choice == 1:
-  string2 = "l(x).if x==4 then k<x>.2 else k<x>.1"
-else:
-  for i in range(nb_choice-1):
-    temp = "if x mod 2 == 0 then "
-    counter = 6+(2*(i)) 
-    for x in range(i+1):
-        temp = temp + "if x<" + str(counter) + " then " 
-        counter -= 2
-    temp = temp + "if x>2 then k<x>.2 else k<x>.1"
-    for x in range(i+1):
-        temp = temp + " else k<x>.1"
-    temp = temp+" else k<x>.1"
-    string2 = string2 + " + ("+temp+")"
+# if nb_choice == 1:
+#   string2 = "l(x).if x==4 then k<x>.2 else k<x>.1"
+# else:
+#   for i in range(nb_choice-1):
+#     temp = "if x mod 2 == 0 then "
+#     counter = 6+(2*(i)) 
+#     for x in range(i+1):
+#         temp = temp + "if x<" + str(counter) + " then " 
+#         counter -= 2
+#     temp = temp + "if x>2 then k<x>.2 else k<x>.1"
+#     for x in range(i+1):
+#         temp = temp + " else k<x>.1"
+#     temp = temp+" else k<x>.1"
+#     string2 = string2 + " + ("+temp+")"
 
-print(string2) 
+# print(string2) 
 
-for i in range(nb_choice-1):
-    string3 = string3 + "+ (k<" + str(i+2) + ">.(l<" +str(i+2)+ ">.X)+(q<" +str(i+2)+ ">.1))"
+# for i in range(nb_choice-1):
+#     string3 = string3 + "+ (k<" + str(i+2) + ">.(l<" +str(i+2)+ ">.X)+(q<" +str(i+2)+ ">.1))"
 
-string3 = string3 + "+(k<"+ str(nb_choice+1) +">.(l<" +str(nb_choice+1)+ ">.X)+(q<" +str(nb_choice+1)+ ">.1))"    
+# string3 = string3 + "+(k<"+ str(nb_choice+1) +">.(l<" +str(nb_choice+1)+ ">.X)+(q<" +str(nb_choice+1)+ ">.1))"    
 
-print(string3)
+# print(string3)
 
 # for i in range(nb_choice-1):
 #     string4 += "k<(x+" +str(i+1)+ ")>."
 # string4 += "1"
 
 # print(string4)
+
+
+inner_t = ""
+for i in range(nb_choice*3):
+  inner_t = inner_t + "(k<" + str(i) + ">.1)+"
+inner_t = inner_t + "(k<" + str(nb_choice*3) + ">.1)"
+
+inner_f = ""
+for i in range(nb_choice*3):
+  inner_f = inner_f + "(k<" + str(i) + ">.2)+"
+inner_f = inner_f + "(k<" + str(nb_choice*3) + ">.2)"
+
+if nb_choice == 1:
+  string4 = "l(x).if x==4 then k<x>." + inner_t + " else k<x>." + inner_f
+else:
+  string4 = "l(x).(if x==4 then k<x>." + inner_t + " else k<x>." + inner_f + ")"
+  for i in range(nb_choice-1):
+    temp = "if x mod 2 == 0 then "
+    temp2 = "(recX.k<1>.X)"
+    counter = 6+(2*(i)) 
+    for x in range(i+1):
+        temp = temp + "if x<" + str(counter) + " then " 
+        counter -= 2
+    temp = temp + "if x>2 then k<x>." + inner_t + " else k<x>." + inner_f
+    for x in range(i+1):
+        temp = temp + " else k<x>." + inner_f
+    temp = temp+" else k<x>." + inner_f
+    string4 = string4 + " + ("+temp+")"
+
+print(string4)
